e6f0ba2eaf3d4705d9603dd4ecac74f90fc9d352
Administrator <admin@example.com>
2024-01-30 14:15:27 +0100

    first commit

---
+++ src/main/java/com/example/projetaback/ProjetABackApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetABackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetABackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projetaback/ProjetABackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetABackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
